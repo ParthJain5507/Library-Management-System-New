0d6c3cd49ed8427b3ddc55188bad04b697c0269c
parth jain <parthjains5507@gmail.com>
2026-08-25 17:17:47 +0530

    update feature

--- main.cpp
+++ main.cpp
@@ -58,6 +58,35 @@ int main() {
         }
         return crow::response(404, "Book not found!");
     });
+    // 4. GET Route: Kisi ek specific ID wali book ko dhundhna
+    CROW_ROUTE(app, "/book/<int>").methods(crow::HTTPMethod::GET)([&library](int id){
+        for(const auto& book : library) {
+            if(book.id == id) {
+                crow::json::wvalue x;
+                x["id"] = book.id;
+                x["title"] = book.title;
+                x["author"] = book.author;
+                return crow::response(200, x);
+            }
+        }
+        return crow::response(404, "Book not found!");
+    });
+
+    // 5. PUT Route: Kisi existing book ki details Update/Edit karna
+    CROW_ROUTE(app, "/book/<int>").methods(crow::HTTPMethod::PUT)([&library](const crow::request& req, int id){
+        auto x = crow::json::load(req.body);
+        if (!x) return crow::response(400, "Invalid JSON");
+
+        for(auto& book : library) {
+            if(book.id == id) {
+                // Naya data set kar rahe hain
+                book.title = x["title"].s();
+                book.author = x["author"].s();
+                return crow::response(200, "Book updated successfully!");
+            }
+        }
+        return crow::response(404, "Book not found!");
+    });
 
     app.port(8080).multithreaded().run();
     return 0;
